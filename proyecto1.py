8c783a32c6e1640504b0b1aed36b07faf8d5b7ab
anasofiahm <anasofiahm1999@gmail.com>
2016-05-27 08:02:58 -0600

    Creación de problema 4

---
+++ proyecto1.py
@@ -0,0 +1,17 @@
+def prob_1 (n):
+	if n %3 == 0
+		return True 
+	else:
+		return False
+def prob_2 (n):
+	return 5/9*(n-32)
+
+def prob_3 (n1,n2):
+	return n1**n2
+
+def prob_4 (n,palabra):
+	cas = (n-len(palabra))//2
+	cas2 = n - (+ len(palabra))
+	return "*" * cas + palabra + "*" * cas2
+
+def prob_5 
